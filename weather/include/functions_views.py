7676e8ceeb8b42b325501aebb24ef7d6b71b97a1
Kamil Korczak <czakix@gmail.com>
2021-04-21 17:28:28 +0200

    feat create Temperature Modell and save temperature data in database. | feat check if chache_time passed before parsing data.

--- weather/include/functions_views.py
+++ weather/include/functions_views.py
@@ -1,29 +1,59 @@
+"""Functions Views"""
+from datetime import date, timedelta
+from django.utils import timezone
+from weather.models import Temperature
 from .weather_parser import weatherParser
 from .temperature_color import temperature_color
 
-from datetime import date
 
 def temperature_data(url, longitude_and_lattitude=None):
     '''Function returns dict with weather data prepared to pass to template.\n 
     Data scraped and parsed using weatherParser.'''
 
-    wp = weatherParser()
-    wp.setUrl(url)
-    wp.setWeatherData()
+    # cache time in minutes
+    cache_time = 15
 
-    today = date.today()
+    parse_date = False
 
-    location = wp.getWeatherLocation()
+    try:
+        temperature = Temperature.objects.get(geographical_coordinates=longitude_and_lattitude)
 
-    current_temperature = wp.getCurrentTemperature()
+        downloaded_date = temperature.last_downloaded
+        date_now = timezone.now()
+
+        if downloaded_date + timedelta(minutes=cache_time) < date_now:
+            parse_date = True
+
+    except Temperature.DoesNotExist:
+        parse_date = True
+
+    if parse_date:
+
+        weather_parser = weatherParser()
+        weather_parser.setUrl(url)
+        weather_parser.setWeatherData()    
+
+        location = weather_parser.getWeatherLocation()
+
+        current_temperature = weather_parser.getCurrentTemperature()
+
+        temperature, created = Temperature.objects.update_or_create( \
+            geographical_coordinates=longitude_and_lattitude,
+            defaults = {
+                'location': location,
+                'current_temp': current_temperature,
+                'temperature_color': temperature_color(current_temperature),
+                'current_icon': weather_parser.getCurrentWeatherIcon()
+            }
+        )
 
     context = {
-        'location': location,
-        'longitude_and_lattitude': longitude_and_lattitude,
-        'current_temp': current_temperature,
-        'temperature_color': temperature_color(current_temperature),
-        'current_icon': wp.getCurrentWeatherIcon(),
-        'current_date': today.strftime("%Y-%m-%d"),
-    }
+            'location': temperature.location,
+            'longitude_and_lattitude': temperature.geographical_coordinates,
+            'current_temp': temperature.current_temp,
+            'temperature_color': temperature.temperature_color,
+            'current_icon': temperature.current_icon,
+            'current_date': date.today().strftime("%Y-%m-%d"),
+        }
 
     return context
--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Temperature(models.Model):
+    location = models.CharField(max_length=200)
+    geographical_coordinates = models.CharField(max_length=200, null=True)
+    current_temp = models.CharField(max_length=5)
+    temperature_color = models.CharField(max_length=3)
+    current_icon = models.CharField(max_length=100)
+    last_downloaded = models.DateTimeField('date downloaded', auto_now=True, blank=True)
