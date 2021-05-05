efc8eb1a315a22bbc7d054e671ef4900bd2ef321
Kamil Korczak <czakix@gmail.com>
2021-05-05 13:57:55 +0200

    refactor urls

--- weather/urls.py
+++ weather/urls.py
@@ -6,14 +6,16 @@ urlpatterns = [
 
     # Task Version
     # 2 URLS referencing to separate views
-    path('temperature/zielona_gora', views.temperature_zielonga_gora, name='zielona-gora'),
+    path('temperature/zielona_gora',
+         views.temperature_zielonga_gora, name='zielona-gora'),
     # GS as Geographical Coordinates
     path('temperature/current/<longitude>,<lattitude>/',
-        views.temperature_geographical_cordinates, name='other-location'),
+         views.temperature_geographical_cordinates, name='other-location'),
 
     # Version 2
     # 2 URLs referencing to one view
-    path('temperature2/zielona_gora', views.temperature_combined, name='zielona-gora2'),
+    path('temperature2/zielona_gora',
+         views.temperature_combined, name='zielona-gora2'),
     path('temperature2/current/<longitude>,<lattitude>/',
-        views.temperature_combined, name='other-location2'),
+         views.temperature_combined, name='other-location2'),
 ]
